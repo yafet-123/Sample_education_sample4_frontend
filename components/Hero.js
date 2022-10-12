4ea3c6afcc9b12eac701f876c00cb53618f2dbc3
yafet-123 <yafetaddisu123@gmail.com>
2022-10-13 01:29:19 +0300

    making mobile responsive

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ export function Hero() {
     <div className="">
       <div className="hero-background w-full h-[40rem]"></div>
       <div className="bg-slate-900 w-full h-screen ">
-        <div className="flex flex-col lg:flex-row mx-20">
+        <div className="hidden lg:flex flex-col lg:flex-row mx-20">
           <div className="bg-white w-96 h-96 mx-auto -mt-20 border rounded-lg p-5 flex flex-col justify-between">
             <h1 className="text-slate-700 text-xl font-extrabold uppercase text-center">
               Programmes
@@ -69,12 +69,12 @@ export function Hero() {
           </div>
         </div>
 
-        <div className="text-white grid grid-cols-1 lg:grid-cols-2 mx-24 lg:my-20 ">
-          <h1 className="text-xl lg:text-5xl font-light capitalize text-left w-full lg:w-1/2">
+        <div className="text-white grid grid-cols-1 lg:grid-cols-2 mx-24 py-20 lg:py-0 lg:my-20">
+          <h1 className="text-3xl lg:text-5xl font-light capitalize text-left w-full lg:w-1/2 mb-10">
             our programs that prompt running
           </h1>
           <div className="flex flex-col justify-between">
-            <p className="text-left font-serif tracking-wide leading-6 capitalize text-base mb-5">
+            <p className="text-left font-serif tracking-wide leading-6 capitalize text-base mb-10">
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
