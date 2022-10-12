013cebee22040fbe8a74516ce8289e16f8940372
yafet-123 <yafetaddisu123@gmail.com>
2022-10-13 01:14:38 +0300

    making mobile responsive

--- components/Trending.js
+++ components/Trending.js
@@ -3,13 +3,13 @@ import React from "react";
 export function Trending() {
   return (
     <div className="bg-slate-800 w-full h-full px-24 py-20">
-      <div className="flex items-center mx-72 relative">
+      <div className="hidden lg:flex items-center mx-72 relative">
         <div className="trending-grid1 w-[30rem] h-[35rem]"></div>
         <div className="trending-grid2 w-[30rem] h-[35rem] absolute top-10 left-[25rem] z-20"></div>
         <div className="hidden lg:block w-[30rem] h-[30rem] bg-pink-400 z-10 absolute top-20 left-[27rem]"></div>
         <div className="trending-grid3 w-[25rem] h-[35rem] absolute -top-10 left-[50rem]"></div>
       </div>
-      <div className="flex flex-col lg:flex-row justify-between mx-20 mt-20">
+      <div className="flex flex-col lg:flex-row justify-between mx-20 mt-10 lg:mt-20">
         <div className="flex flex-col justify-between items-center">
           <h1 className="text-white text-5xl font-normal capitalize text-left mb-5 tracking-wide w-full lg:w-2/4">
             top running trends of 2021
