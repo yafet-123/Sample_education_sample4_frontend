199ba385476d3a3d24fc76f5a8cef73a83b439b5
yafet-123 <yafetaddisu123@gmail.com>
2022-10-13 01:19:37 +0300

    making mobile responsive

--- components/About.js
+++ components/About.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export function About() {
   return (
-    <div className="bg-slate-300 px-24 h-screen w-full">
-      <div className="about-background w-3/4 h-1/2 mt-16 float-right relative">
-        <div className="bg-slate-800 w-[40rem] h-[25rem] p-10 absolute top-2/4 -left-52">
+    <div className="bg-slate-300 px-0 lg:px-24 h-screen w-full">
+      <div className="about-background w-full lg:w-3/4 h-full lg:h-1/2 mt-0 lg:mt-16 float-right relative">
+        <div className="hidden lg:block bg-slate-800 w-[40rem] h-[25rem] p-10 lg:absolute lg:top-2/4 lg:-left-52">
           <h1 className="text-white text-2xl font-light capitalize text-left mb-5 tracking-wide">
             about us
           </h1>
@@ -14,7 +14,7 @@ export function About() {
           </h1>
         </div>
 
-        <div className="bg-white w-[40rem] h-[30rem] p-10 absolute top-3/4 right-1/4">
+        <div className="bg-white w-[40rem] h-[30rem] p-10 absolute top-10 lg:top-3/4 right-10 lg:right-1/4">
           <h1 className="text-black text-4xl font-light capitalize text-center mb-5 tracking-wide">
             contact us
           </h1>
